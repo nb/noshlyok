b176e28ce26fc31e6bcf69354cf5d8a4cbe5a3d4
nbachiyski <nbachiyski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-02 15:11:30 +0000

    * Check author field for Russian letters  * Reject .ua domains, too
    git-svn-id: http://plugins.svn.wordpress.org/noshlyok/trunk@159975 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- noshlyok.php
+++ noshlyok.php
@@ -5,7 +5,7 @@ Plugin URI: http://wordpress.org/extend/plugins/noshlyok/
 Description: Не позволява изпращането на коментари без поне един кирилишки символ
 Author: Николай Бачийски
 Author URI: http://nikolay.bg/
-Version: 0.05
+Version: 0.06
 License: The source code below is in the public domain
 */ 
 
@@ -23,22 +23,27 @@ function noshlyok_shlyok_allowed($post_id) {
 
 function noshlyok_verify($comment_data) {
 	
+	$top_level_domain_re = '/\.(ru|ua)$/';
+	$cyrillic_letters_re = '/[а-яА-Я]/u';
+	$no_russian_letters_re = '/ы|ё|э|Ы|Ё|Э/';
+	
 	if ( noshlyok_shlyok_allowed($comment_data['comment_post_ID']) ) {
 		return $comment_data;
 	}
 	
 	// do not allow comments without cyrillic characters
-    if (!preg_match("/[а-яА-Я]/u", $comment_data['comment_content'])) {
-		noshlyok_die("Моля, пишете на кирилица!</p><p>Please use cyrillic letters for your comment!");
+    if (!preg_match($cyrillic_letters_re, $comment_data['comment_content'])) {
+		noshlyok_die("Моля, пишете на кирилица!</p><p>Please, use cyrillic letters for your comment!");
     }
 	// do not allow .ru emails and web sites
 	if (!preg_match('|^http://|', $comment_data['comment_author_url']) && $comment_data['comment_author_url']) {
 		$comment_data['comment_author_url'] = 'http://'.$comment_data['comment_author_url'];
 	}
     $parsed = @parse_url($comment_data['comment_author_url']);
-    if ( (isset($parsed['host']) && preg_match("|\.ru$|", $parsed['host'])) ||
-			preg_match("|\.ru$|", $comment_data['comment_author_email']) || 
-			preg_match("/ы|ё|э/i", $comment_data['comment_content'])) {
+    if ( (isset($parsed['host']) && preg_match($top_level_domain_re, $parsed['host'])) ||
+			preg_match($top_level_domain_re, $comment_data['comment_author_email']) || 
+			preg_match($no_russian_letters_re, $comment_data['comment_content']) ||
+			preg_match($no_russian_letters_re, $comment_data['comment_author'])) {
         noshlyok_die('Русский &mdash; нет!');
 	}
 
